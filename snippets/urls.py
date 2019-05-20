b809085e2185e4ce033c5b9cc723672eda019575
WCW <1@1.com>
2019-05-20 21:14:48 +0800

    程序更新

--- snippets/urls.py
+++ snippets/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     # path('<int:pk>/highlight/', SnippetHighlight.as_view(), name='highlight'),
 
     path('', SnippetViewSet.as_view({'get': 'list', 'post': 'create'}), name='snippets-list'),
-    path('<int:pk>/', SnippetViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='snippet-detail'),
+    path('<int:pk>/', SnippetViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='snippet-detail'),
     path('<int:pk>/highlight/', SnippetViewSet.as_view({'get': 'get_highlight'}, renderer_classes=[StaticHTMLRenderer]), name='snippet-highlight'),
 
 ]
